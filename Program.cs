476f10a9184600b3d74d1e2de5f6d23638c14e2c
EmirCGN <ritterskiller@gmail.com>
2022-10-14 02:14:08 +0200

    Playing field and a check who has won, moreover variables were added Declared and initalized

--- Program.cs
+++ Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace Tictactoe {
+
+    class Programm
+    {
+        static string player1 = "";
+        static string player2 = "";
+        static char[] playField = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int repeat = 0;
+        static int choice;
+        static bool playing = true;
+        static bool flag = false, correntInput = false;
+
+        static void Main(string[] args) //Main Methode
+        {
+            Console.WriteLine("Wie heißt Spieler-1?");
+            player1 = Console.ReadLine();
+            Console.WriteLine("Wie heißt Spieler-2?");
+            player2 = Console.ReadLine();
+            Console.WriteLine("{0} ist X und {1} ist O", player1, player2);
+            Console.WriteLine("{0} fängt an!", player1);
+            Console.ReadLine();
+            Console.Clear();
+
+            while(playing == true)
+            {
+                while(flag = false)
+                {
+                    Field();
+                }
+            }
+        }
+
+        public static void Field() //Spielfeld Methode
+        {
+            Console.Clear();
+            Console.WriteLine("     |     |      "); 
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[1], playField[2], playField[3]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[4], playField[5], playField[6]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[7], playField[8], playField[9]); 
+            Console.WriteLine("     |     |      ");
+            repeat++;
+        }
+
+        public static bool CheckWin() //Win Prüfer Methode
+        {
+            //Horizontal 
+            if (playField[1] == playField[2] && playField[2] == playField[3]) //(Erste Reihe)
+            {
+                return true;
+            }
+            else if (playField[4] == playField[5] && playField[5] == playField[6]) //(Zweite Reihe)
+            {
+                return true;
+            }
+            else if (playField[6] == playField[7] && playField[7] == playField[8]) //(Dritte Reihe)
+            {
+                return true;
+            }
+            //Vertikal
+            else if (playField[1] == playField[4] && playField[4] == playField[7]) //(Erste Reihe)
+            {
+                return true;
+            }
+            else if (playField[2] == playField[5] && playField[5] == playField[8]) //(Zweite Reihe)
+            {
+                return true;
+            }
+            else if (playField[3] == playField[6] && playField[6] == playField[9]) //(Dritte Reihe)
+            {
+                return true;
+            }
+            //Diagonal 
+            else if (playField[1] == playField[5] && playField[5] == playField[9])
+            {
+                return true;
+            }
+            else if (playField[3] == playField[5] && playField[5] == playField[7])
+            {
+                return true;
+            }
+            //Unentschieden Prüfer
+            else if (playField[1] != '1' && playField[2] != '2' && playField[3] != '3' && playField[4] != '4' && playField[5] != '5' && playField[6] != '6' && playField[7] != '7' && playField[8] != '8' && playField[9] != '9')
+            {
+                return false;
+            }
+        }
+    }
+}
