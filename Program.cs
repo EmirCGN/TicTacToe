41b5f98fa02008b0fc4b8b2494599beb67b056a3
EmirCGN <ritterskiller@gmail.com>
2022-10-27 14:57:31 +0200

    Scores and a loop have been added

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 ﻿
 
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks.Sources;
+
 namespace Tictactoe {
 
     class Programm
     {
         static string player1 = "";
         static string player2 = "";
+        static int score1 = 0, score2 = 0;
         static char[] playField = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         static int repeat = 0;
         static int choice;
+        static int turn = 1;
         static bool playing = true;
         static bool flag = false, correntInput = false;
 
@@ -25,9 +30,50 @@ namespace Tictactoe {
 
             while(playing == true)
             {
-                while(flag = false)
+                while(flag = false) // Game-Loop
                 {
                     Field();
+                    Console.WriteLine("");
+
+                    Console.WriteLine("Score: {0} - {1}     {2} - {3}", player1, score1, player2, score2);
+                    if (turn== 1)
+                    {
+                        Console.WriteLine("{0} ist dran (X) ", player1);
+                    }
+                    if (turn == 2)
+                    {
+                        Console.WriteLine("{0} ist dran (O)", player2);
+                    }
+                    while(correntInput == false)
+                    {
+                        Console.WriteLine("Bitte wählen Sie ein Feld aus!");
+                        choice = int.Parse(Console.ReadLine());
+                        if (choice > 0 && choice < 9)
+                        {
+                            correntInput = true;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    correntInput = false; //Zurücksetzen
+
+                    if (turn == 1)
+                    {
+                        if (playField[choice] == "X") //Überprüft ob das Feld besetzt ist
+                        {
+                            Console.WriteLine("Dieser Feld ist bereits besetzt!");
+                            Console.ReadLine();
+                            Console.Clear();
+                            continue;
+                        }
+                        else
+                        {
+
+                        }
+                    }
                 }
             }
         }
@@ -35,11 +81,15 @@ namespace Tictactoe {
         public static void Field() //Spielfeld Methode
         {
             Console.Clear();
+            Console.WriteLine("     |     |      ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", playField[1], playField[2], playField[3]);
-            Console.WriteLine("-------------------");
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", playField[4], playField[5], playField[6]);
-            Console.WriteLine("-------------------");
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", playField[7], playField[8], playField[9]);
+            Console.WriteLine("     |     |      ");
             repeat++;
         }
 
@@ -87,7 +137,6 @@ namespace Tictactoe {
             }
             return false;
 
-            return false;
         }
     }
 }
